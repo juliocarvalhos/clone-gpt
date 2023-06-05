430e2f74694e82562597108e754e06209d3895a4
jcsouza <julio_souza@id.uff.br>
2023-06-05 15:55:44 -0300

    adicionando rota de logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   resources :messages
   resources :conversations
 end
